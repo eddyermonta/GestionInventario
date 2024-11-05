596939437de3a0c4e04587da60eb053e083ed6e9
eddyermonta <eduardo.montano@utp.edu.co>
2024-11-05 04:25:23 -0500

    Agrega documentación XML para el método de validación de usuario en AuthController, especificando parámetros y respuestas.

--- src/Modules/Auths/Controllers/AuthController.cs
+++ src/Modules/Auths/Controllers/AuthController.cs
@@ -9,6 +9,18 @@ public class AuthController(IAuthService authService) : ControllerBase{
 
     private readonly IAuthService _authService = authService;
 
+    /// <summary>
+    ///   Validates if the user exists in the database.
+    /// </summary>
+    /// <param name="authRequest">
+    ///  Object containing the user's email and password.
+    ///  </param>
+    /// <returns>
+    ///  Returns an AuthResponse object with the authentication token.
+    /// </returns>
+    ///  <response code="200">Returns an AuthResponse object with the authentication token.</response>
+    ///  <response code="401">Returns an AuthResponse object with an incorrect response.</response>
+    /// <response code="400">Returns a bad request for incorrect parameters.</response>
     [HttpPost("validate")]
     [ProducesResponseType(typeof(AuthResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
